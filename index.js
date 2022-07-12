12af6988c42a025d71734876cd2d6fb28cc0ba0d
efficientprogramming01 <efficient.contacts@gmail.com>
2022-07-12 18:46:16 +0200

    updated

--- index.js
+++ index.js
@@ -93,6 +93,27 @@ app.get('/lte/:id',async(req,res)=>{  // here we get query string eg: localhost:
  
 })
 
+
+app.put('/lte/:id',async(req,res)=>{  // here we get query string eg: localhost:3000/lte/:id?name=papy&age=15 WIL GIVE REPONSE {"name":"papy","age":"15"}
+
+    // console.log(JSON.stringify(req.query))
+    lteData = req.query
+   // res.status(200).send(JSON.stringify(req.query))
+    MongoClient.connect(url, function(err, db) {
+       if (err) throw err;
+       var dbo = db.db(db_name);
+       var myobj = {thermalImage:lteData,updatedAt:new Date,createdAt:new Date()};
+       dbo.collection(thermal_image_collection).insertOne(myobj, function(err, resp) {
+         if (err) throw err;
+          res.send({resp})
+         db.close();
+         
+       });
+     });
+    
+   })
+
+
 app.get('/lte',async(req,res)=>{
 
     MongoClient.connect(url, function(err, db) {
